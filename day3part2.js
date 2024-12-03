01e88ea6614c8ac8a134686884a55aa2545f1c71
zmejchik <zmejchik@gmail.com>
2024-12-03 13:25:26 +0200

    3 2

---
+++ day3part2.js
@@ -0,0 +1,25 @@
+let result = 0;
+
+const regexForTrueMultiple = /mul\((-?\d+(\.\d+)?),(-?\d+(\.\d+)?)\)/g;
+const regexNumber = /(-?\d+(\.\d+)?)/g;
+const regexDontDo = /don't\(\)([\s\S]*?)do\(\)/g;
+
+getData("./inputday3part1.txt").then((data) => {
+  data = data.replace(regexDontDo, "");
+
+  console.log(data);
+  const trueMultiply = data.match(regexForTrueMultiple);
+
+  trueMultiply.forEach((oneFormula) => {
+    const [number1, number2] = oneFormula.match(regexNumber);
+    result += parseFloat(number1) * parseFloat(number2);
+  });
+
+  console.log(result);
+});
+
+async function getData(pathToFile) {
+  const response = await fetch(pathToFile);
+  const data = await response.text();
+  return data;
+}
