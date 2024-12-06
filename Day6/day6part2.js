21bd88d04a786c4b190c2b1c3aff24d387dc8324
zmejchik <zmejchik@gmail.com>
2024-12-06 18:17:35 +0200

    6

--- Day6/day6part2.js
+++ Day6/day6part2.js
@@ -1,6 +1,7 @@
-import { initialDate } from "./inputday6Ex.js";
+import { initialDate } from "./inputday6.js";
 
 const data = initialDate.split("\n").map((row) => row.split(""));
+const data2 = initialDate.split("\n").map((row) => row.split(""));
 const rows = data.length;
 const columns = data[0].length;
 
@@ -9,32 +10,18 @@ const EMPTY = ".";
 const WALL = "#";
 const HEREWAS = "X";
 
-let result = 1;
 const positionGuard = findPositionGuard();
 const directionName = ["up", "right", "down", "left"];
 let directionIndex = 0;
 let [nextX, nextY] = [positionGuard.x, positionGuard.y];
+let visitedPositions = [];
+visitedPositions.push({ x: positionGuard.x, y: positionGuard.y });
+let result = 1;
+let result2 = 1;
 
-function moveGuard() {
-  switch (directionName[directionIndex]) {
-    case "up":
-      nextY--;
-      break;
-    case "right":
-      nextX++;
-      break;
-    case "down":
-      nextY++;
-      break;
-    case "left":
-      nextX--;
-      break;
-  }
-}
-
+//first goings position
 while (isValidPosition(nextX, nextY)) {
   moveGuard();
-
   if (isValidPosition(nextX, nextY) && data[nextY][nextX] === WALL) {
     switch (directionName[directionIndex]) {
       case "up":
@@ -56,15 +43,76 @@ while (isValidPosition(nextX, nextY)) {
   if (isValidPosition(nextX, nextY) && data[nextY][nextX] === EMPTY) {
     data[nextY][nextX] = HEREWAS;
     result++;
+    visitedPositions.push({ x: nextX, y: nextY });
   }
 }
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function findLoopPosition(data2, directionName) {
+  let newData = data2;
+  for (let i = 0; i < newData.length; i++) {
+    for (let j = 0; j < newData[i].length; j++) {
+      if (data[i][j] === HEREWAS) {
+        let count = 0;
+        newData[i][j] = WALL;
+        [nextX, nextY] = [positionGuard.x, positionGuard.y];
+        while (isValidPosition(nextX, nextY) && count < 1000000) {
+          moveGuard();
+          if (isValidPosition(nextX, nextY) && newData[nextY][nextX] === WALL) {
+            switch (directionName[directionIndex]) {
+              case "up":
+                nextY++;
+                break;
+              case "right":
+                nextX--;
+                break;
+              case "down":
+                nextY--;
+                break;
+              case "left":
+                nextX++;
+                break;
+            }
+            directionIndex = (directionIndex + 1) % 4;
+          }
+          count++;
+          if (count === 1000000) {
+            result2++;
+          }
+        }
+        newData[i][j] = HEREWAS;
+        directionIndex = 0;
+      }
+    }
+  }
+  return result2;
+}
+
+//================================================================
+result2 = findLoopPosition(data2, directionName);
 
 console.log(result);
+console.log(result2);
 
 function isValidPosition(x, y) {
   return x >= 0 && x < columns && y >= 0 && y < rows;
 }
 
+function moveGuard() {
+  switch (directionName[directionIndex]) {
+    case "up":
+      nextY--;
+      break;
+    case "right":
+      nextX++;
+      break;
+    case "down":
+      nextY++;
+      break;
+    case "left":
+      nextX--;
+      break;
+  }
+}
 function findPositionGuard() {
   for (let y = 0; y < rows; y++) {
     const x = data[y].indexOf(GUARD);
